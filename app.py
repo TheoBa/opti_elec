0d7b94aeb1746007263a3ea34e0e6de2d5029970
Theo Badoz <theo.badoz@gmail.com>
2025-01-13 10:40:51 -0500

    feat: create a time series display for a PoC

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from utils.get_data import get_data, parse_data_string, build_df
+from utils.display import display_time_series
 import pandas as pd
 import datetime as dt
-
 
 ENTITY_IDS = {
     "capteur_chambre_temperature": "sensor",
@@ -16,8 +16,8 @@ st.set_page_config(
     page_icon='😎', 
     layout="wide", 
     initial_sidebar_state="collapsed"
-    )
- 
+)
+
 def welcome_page():
     st.markdown("""# PoC OptiElec""")
     button = st.button("get data")
@@ -25,7 +25,9 @@ def welcome_page():
         for entity_id, prefix in ENTITY_IDS.items():
             data = get_data(entity_id, is_sensor=(prefix == 'sensor'))
             st.dataframe(build_df(data, prefix))
-
+    
+    # Display time series plots
+    display_time_series()
 
 if __name__=="__main__":
     welcome_page()
