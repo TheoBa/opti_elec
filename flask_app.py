ec76f8bf88576153a01bd0bc9e03220b1551c57a
Theo Badoz <theo.badoz@gmail.com>
2025-03-14 14:14:22 -0400

    refacto: 3 POST APIs

--- flask_app.py
+++ flask_app.py
@@ -29,13 +29,56 @@ def compute_simulation():
     # Initialize the Simulation class
     simu = Simulation(module_config, mode="forecasted", parameters=parameters, scenario=heating_scenario)
 
-    # Get the simulation DataFrame
-    simulation_df = simu.simulation_df
+    # Prepare the response
+    response = {
+        'temperature_evolution': simu.simulation_df[["date", "T_int_pred"]].to_dict(orient='records')
+    }
+
+    return jsonify(response)
+
+@app.route('/get_parameters', methods=['POST'])
+def get_parameters():
+    data = request.json
+    module_name = data.get('module_name')
+
+    if not module_name:
+        return jsonify({'error': 'module_name is required'}), 400
+    
+    # Find best model's parameters
+    log_runs = prepare_logs()
+    logs_df = log_runs[log_runs['module_name'] == module_name][["date", "R", "C", "alpha", "Pvoisin", "time_shift"]]
+    parameters = logs_df.sort_values(by='date', ascending=False).reset_index(drop=True).loc[0, ["R", "C", "alpha", "Pvoisin", "time_shift"]].values.flatten().tolist()
 
     # Prepare the response
     response = {
-        #'temperature_evolution': simu.simulation_df[["date", "T_int_pred"]].to_dict(orient='records'),
-        'parameters': simu.parameters,
+        'parameters': parameters
+    }
+
+    return jsonify(response)
+
+@app.route('/compute_consomption', methods=['POST'])
+def compute_consomption():
+    data = request.json
+    module_name = data.get('module_name')
+    heating_scenario = data.get('heating_scenario')
+
+    if not module_name or not heating_scenario:
+        return jsonify({'error': 'module_name and heating_scenario are required'}), 400
+
+    # Load module configuration (adjust this based on your actual configuration loading mechanism)
+    config = json.load(open("config.json", "r"))
+    module_config = config[module_name]
+
+    # Find best model's parameters
+    log_runs = prepare_logs()
+    logs_df = log_runs[log_runs['module_name'] == module_name][["date", "R", "C", "alpha", "Pvoisin", "time_shift"]]
+    parameters = logs_df.sort_values(by='date', ascending=False).reset_index(drop=True).loc[0, ["R", "C", "alpha", "Pvoisin", "time_shift"]].values.flatten().tolist()
+
+    # Initialize the Simulation class
+    simu = Simulation(module_config, mode="forecasted", parameters=parameters, scenario=heating_scenario)
+
+    # Prepare the response
+    response = {
         'consumption': simu.compute_scenarios_consumption()
     }
 
